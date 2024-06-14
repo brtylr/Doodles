8931e9056618db6481f9f0b3bee7df82616f7300
Bree Taylor <60380333+brtylr@users.noreply.github.com>
2024-06-14 00:04:32 -0400

    Update Copy My Scans.bat
    debugged a bit

--- Copy My Scans.bat
+++ Copy My Scans.bat
@@ -1,44 +1,52 @@
-@echo off
 Rem Clears command prompt for readability.
+@echo off
+cls
 
 :start
+
 
 Rem Req is scanned and the input is set to scanID variable.
 Set /P scanID=Please scan or type requisition now.
 
-@echo off
+Set pathmain="S:\Doctor Reqs"
+cls
 
 Rem Checks for pdf file in S:\Doctor Reqs\ with scanID name. Repeats script from start if none found.
-IF NOT EXIST “S:\Doctor Reqs\%scanID%.pdf” (
+IF NOT EXIST %pathmain%\%scanID%.pdf (
 	echo PDF for %scanID% not found! Batch script is set to repeat.
 	pause
+	cls
 	goto start
 )
 
 Rem scanID.pdf is copy and pasted. /Y forces an overwrite. Error is produced if copies are not found in destination folder. Three folders are checked.
 Set errorflag=0
 
-Copy /Y S:\Doctor Reqs\%scanID%.pdf S:Doctor Reqs\Michigan Reqs\
 
-IF NOT EXIST “S:\Doctor Reqs\Michigan Reqs\%scanID%.pdf” (
+Set subpath1="Michigan Reqs"
+Copy /Y %pathmain%\%scanID%.pdf %pathmain%\%subpath1%\
+
+IF NOT EXIST %pathmain%\%subpath1%\%scanID%.pdf (
 	echo %scanID% not found in Michigan Reqs!
 	set errorflag=1
 ) ELSE (
 	echo %scanID% copied to Michigan Reqs.
 )
 
-Copy /Y S:\Doctor Reqs\%scanID%.pdf S:Doctor Reqs\Phillips Reqs\
+Set subpath2="Phillips Reqs"
+Copy /Y %pathmain%\%scanID%.pdf %pathmain%\%subpath2%\
 
-IF NOT EXIST “S:\Doctor Reqs\Phillips Reqs\%scanID%.pdf” (
+IF NOT EXIST %pathmain%\%subpath2%\%scanID%.pdf (
 	echo %scanID% not found in Doctor Reqs!
 	set errorflag=1
 ) ELSE (
 	echo %scanID% copied to Doctor Reqs.
 )
 
-Copy /Y S:\Doctor Reqs\%scanID%.pdf S:Doctor Reqs\IntelliPath PDF Requisitions\
+Set subpath3="IntelliPath PDF Requisitions"
+Copy /Y %pathmain%\%scanID%.pdf %pathmain%\%subpath3%\
 
-IF NOT EXIST “S:\Doctor Reqs\IntelliPath PDF Requisitions\%scanID%.pdf” (
+IF NOT EXIST %pathmain%\%subpath3%\%scanID%.pdf (
 	echo %scanID% not found in IntelliPath PDF Requisitions!
 	set errorflag=1
 ) ELSE (
@@ -50,6 +58,6 @@ IF errorflag==1 (
 )
 
 Rem Opens scanID pdf file in C:\Program Files (x86)\Adobe\Acrobat Reader\Reader\AcroRd32.exe
-Start “” “S:\Doctor Reqs\%scanID%.pdf”
+Start %pathmain%\%scanID%.pdf
 
 Goto start
